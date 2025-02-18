f1e064b8d3625be83620d1cc48fba09ca9c5cf36
hslnoni-tho <9892@holbertonstudents.com>
2025-02-18 09:43:25 +0100

    fixm my github problem

---
+++ python-input_output/10-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Defines a Student class."""
+
+
+class Student:
+    """Represent a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new Student."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Get dictionary representation of
+        Student with attribute filtering."""
+        if attrs is None:
+            return self.__dict__
+        return {k: v for k, v in self.__dict__.items() if k in attrs}
+
+    def reload_from_json(self, json):
+        """Method that replaces all attributes of the Student instance."""
+        for key, value in json.items():
+            setattr(self, key, value)
